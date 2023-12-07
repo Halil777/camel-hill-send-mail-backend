f16b42e7c07364798c5c72790bd3733978fcfaa5
Halil777 <microsoft7779@gmail.com>
2023-12-07 18:14:15 +0500

    change ip

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post("/send-email", async (req, res) => {
     }
 
     const mailOptions = {
-      from: process.env.EMAIL_USER,
+      from: "sendtomail777999@gmail.com",
       to: "info@duyelidepe.com",
       subject: "New Contact Form Submission",
       text: `
